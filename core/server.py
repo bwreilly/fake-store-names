31b6f352cd9c93264e3760ca655e1f1d4918452f
Ben Reilly <benwreilly@gmail.com>
2016-10-02 09:49:02 -0700

    and it's working cool

--- core/server.py
+++ core/server.py
@@ -3,7 +3,10 @@ from flask_restful import Resource, Api
 
 from generate import new_store
 
+FRONTEND = "http://localhost:8000"
+
 app = Flask(__name__)
+# app.config.from_object('config')
 api = Api(app)
 
 class StoreName(Resource):
@@ -11,6 +14,13 @@ class StoreName(Resource):
     def get(self):
         name = new_store()
         return {'name': name}
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', FRONTEND)
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
 
 api.add_resource(StoreName, '/', '/store/new/')
 
