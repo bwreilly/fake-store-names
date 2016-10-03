ba859e62bd1f6edbcc2dc8aaf0a7b94c3f152452
Ben Reilly <benwreilly@gmail.com>
2016-10-02 19:36:01 -0700

    CORS for the CORS god

--- core/server.py
+++ core/server.py
@@ -17,7 +17,7 @@ class StoreName(Resource):
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', "*")
+  response.headers.add('Access-Control-Allow-Origin', FRONTEND)
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   return response
